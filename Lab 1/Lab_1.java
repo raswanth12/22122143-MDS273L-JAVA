1db145f6b3c67847b0b4bd07eb25a24fa736b64e
Raswanth <83648180+raswanth12@users.noreply.github.com>
2023-02-22 10:35:21 +0530

    Update Lab_1.java

--- Lab 1/Lab_1.java
+++ Lab 1/Lab_1.java
@@ -10,54 +10,54 @@ Assume that you can divide the states among India into four different regions (N
   public class Lab_1 {
      public static void main(String[] args) {
 
-      Scanner scan = new Scanner(System.in);
+      try (Scanner scan = new Scanner(System.in)) {
+        System.out.println("WELCOME YOU ! ");
 
-      System.out.println("WELCOME YOU ! ");
+          System.out.println("Enter your name: ");
+          String name = scan.nextLine();
 
-      System.out.println("Enter your name: ");
-      String name = scan.nextLine();
+          System.out.println("Enter your age: ");
+          int age = scan.nextInt();
+          
+          System.out.println("Enter your gender (Male=m/Female=f): ");
+          char gender = scan.next().charAt(0);
 
-      System.out.println("Enter your age: ");
-      int age = scan.nextInt();
-      
-      System.out.println("Enter your gender (Male=m/Female=f): ");
-      char gender = scan.next().charAt(0);
+          System.out.println("Enter your state : ");
+          String region = scan.next();
 
-      System.out.println("Enter your state : ");
-      String region = scan.next();
+          System.out.println("Name: " + name);
+          System.out.println("Age: " + age);
+          
+          if (gender == 'm') {
+            System.out.println("Gender: MALE");
+        } else {
+            System.out.println("Gender: FEMALE");
+        }
 
-      System.out.println("Name: " + name);
-      System.out.println("Age: " + age);
-      
-      if (gender == 'm') {
-        System.out.println("Gender: MALE");
-    } else {
-        System.out.println("Gender: FEMALE");
-    }
+        switch (region) {
+            case "andhra pradesh", "karnataka", "kerala", "tamilnadu", "telangana":
+                System.out.println("The student is from the southern states of India.");
+                break;
 
-    switch (region) {
-        case "andhra pradesh", "karnataka", "kerala", "tamilnadu", "telangana":
-            System.out.println("The student is from the southern states of India.");
-            break;
+            case "Jammu & Kashmir", "Himachal Pradesh", "Punjab", "Uttarakhand", "Haryana", "Delhi", "Rajasthan", "Uttar Pradesh" ,"UT Chandigarh":
+                System.out.println("The student is from the northern states of India.");
+                break;
 
-        case "Jammu & Kashmir", "Himachal Pradesh", "Punjab", "Uttarakhand", "Haryana", "Delhi", "Rajasthan", "Uttar Pradesh" ,"UT Chandigarh":
-            System.out.println("The student is from the northern states of India.");
-            break;
+            case "Arunachal Pradesh", "Assam", "Manipur", "Meghalaya", "Mizoram", "Nagaland","Tripura":
+                System.out.println("The student is from the eastern states of India.");
+                break;
 
-        case "Arunachal Pradesh", "Assam", "Manipur", "Meghalaya", "Mizoram", "Nagaland","Tripura":
-            System.out.println("The student is from the eastern states of India.");
-            break;
+            case "Chhattisgarh", "Odisha", "Sikkim", "West Bengal":
+                System.out.println("The student is from the western states of India.");
+                break;
 
-        case "Chhattisgarh", "Odisha", "Sikkim", "West Bengal":
-            System.out.println("The student is from the western states of India.");
-            break;
-
-        default:
-            System.out.println("Invalid region.");
-            break;
+            default:
+                System.out.println("Invalid region.");
+                break;
 
 
-            
+                
+        }
     }
 
       
