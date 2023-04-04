ef190780237ae55b1cb04efbda77690be3b75158
Raswanth <83648180+raswanth12@users.noreply.github.com>
2023-04-04 08:07:00 +0530

    Update readtextfile.java

--- Lab 6/readtextfile.java
+++ Lab 6/readtextfile.java
@@ -1,7 +1,10 @@
-    import java.io.BufferedReader;
-    import java.io.FileReader;
-    import java.io.IOException;
-    import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
     public class readtextfile {
@@ -56,8 +59,55 @@
         System.out.println("Vowels and their count:");
         System.out.println(vowels);
         System.out.println("Digits and their count:");
-        System.out.println(digits);
+        System.out.println(digits);}
+      // Change the file path to the location of your text file
+        String filePath = "text.txt";
+
+        Map<String, Integer> wordCounts = new HashMap<>();
+        Map<Character, Integer> charCounts = new HashMap<>();{
+
+        
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+        }BufferedReader br;
+        String line;
+        while ((line = br.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    wordCounts.merge(word, 1, Integer::sum);
+                    for (char c : word.toCharArray()) {
+                        charCounts.merge(c, 1, Integer::sum);
+                    }
+                }
+            }
+        }
+
+        List<Map.Entry<String, Integer>> wordList = new ArrayList<>(wordCounts.entrySet());
+        wordList.sort(Map.Entry.<String, Integer>comparingByValue().reversed());
+        List<Map.Entry<Character, Integer>> charList = new ArrayList<>(charCounts.entrySet());
+        charList.sort(Map.Entry.<Character, Integer>comparingByValue().reversed());
+
+        System.out.println("Top five repeated words and their count:");
+        for (int i = 0; i < 5 && i < wordList.size(); i++) {
+            System.out.println(wordList.get(i).getKey() + ": " + wordList.get(i).getValue());
+        }
+        System.out.println("Least five repeated words and their count:");
+        for (int i = wordList.size() - 1; i >= wordList.size() - 5 && i >= 0; i--) {
+            System.out.println(wordList.get(i).getKey() + ": " + wordList.get(i).getValue());
+        }
+
+        System.out.println("Top five repeated characters and their count:");
+        for (int i = 0; i < 5 && i < charList.size(); i++) {
+            System.out.println(charList.get(i).getKey() + ": " + charList.get(i).getValue());
+        }
+        System.out.println("Least five repeated characters and their count:");
+        for (int i = charList.size() - 1; i >= charList.size() - 5 && i >= 0; i--) {
+            System.out.println(charList.get(i).getKey() + ": " + charList.get(i).getValue());
+        }
     }
-    }
+
+
+    
 
     
